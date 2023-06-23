7f5ee9a0919d078026b860e7d02cf5c7ba848f34
kuzeycaliskan <kuzeycaliskann@gmail.com>
2023-06-23 20:02:01 +0300

    models created

--- src/main/java/com/folksdev/account/model/Customer.kt
+++ src/main/java/com/folksdev/account/model/Customer.kt
@@ -1,3 +1,30 @@
 package com.folksdev.account.model
 
-data class Customer()
+import org.hibernate.annotations.GenericGenerator
+import java.math.BigDecimal
+import java.time.LocalDateTime
+import javax.persistence.*
+
+
+/***
+ * Kotlinde nesneyi olustururken sadece constructor kullanabiliyoruz. set get yok.
+ * val kullandigimiz icin bu sekildedir.
+ */
+@Entity
+data class Customer(
+
+        @Id
+        @GeneratedValue(generator = "UUID")
+        @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+        val id: String?,
+
+        val name:String?,
+        val surname:String?,
+
+        /***
+         * Ben customer'i cektigimde customer'a ait accountlari da cekebilmek istiyorum.
+         * O yuzden one to many kullandik.
+         */
+        @OneToMany(mappedBy = "customer", fetch = FetchType.LAZY)
+        val account: Set<Account>?
+)
--- src/main/java/com/folksdev/account/model/Transaction.kt
+++ src/main/java/com/folksdev/account/model/Transaction.kt
@@ -1,3 +1,29 @@
 package com.folksdev.account.model
 
-data class Transaction()
+import org.hibernate.annotations.GenericGenerator
+import java.math.BigDecimal
+import java.time.LocalDateTime
+import javax.persistence.*
+
+@Entity
+data class Transaction(
+
+
+        @Id
+        @GeneratedValue(generator = "UUID")
+        @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+        val id: String?,
+        val transactionType: TransactionType? = TransactionType.INITIAL, //default INITIAL olacak
+        val amount: BigDecimal?, //double yerine BigDecimal kullanilmali
+        val transactionDate: LocalDateTime,
+
+        //Transaction dan account bilgisine erisebilmek icin
+        @ManyToOne(fetch = FetchType.LAZY, optional = false, cascade = [CascadeType.ALL])
+        @JoinColumn(name = "account_id", nullable = false)
+        val account: Account
+
+)
+
+enum class TransactionType{
+    INITIAL, TRANSFER
+}
