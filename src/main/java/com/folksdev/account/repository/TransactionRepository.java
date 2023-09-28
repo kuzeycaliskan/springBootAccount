7c37b37bad0b20ef83d2f4df33188927fde84eb5
kuzeycaliskan <kuzeycaliskann@gmail.com>
2023-09-28 23:51:48 +0300

    folder design with fe project

--- src/main/java/com/folksdev/account/repository/TransactionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.folksdev.account.repository;
-
-import com.folksdev.account.model.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransactionRepository extends JpaRepository<Transaction, String> {
-}
