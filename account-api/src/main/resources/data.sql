5aed8f75415d9eea0ab6839c7716105d84f54ec7
kuzeycaliskan <kuzeycaliskann@gmail.com>
2025-04-11 00:20:00 +0300

    Mysql issue solved

---
+++ account-api/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO `customer` VALUES ('bb180e18-b84e-4906-ba6c-6cea1301f074','Burak','Gunduz'),('e63bdd53-ec6e-4fbf-9f3e-0fd47d83a1a2','Kuzey','Caliskan'),('ed8dbead-2d90-4672-8054-61560286cfc1','Leyla','Sahverdi');
+INSERT INTO `account` VALUES ('193dd0a0-bb76-4831-9ad9-9f8eded5e624',0.00,'2024-05-19 15:25:31','ed8dbead-2d90-4672-8054-61560286cfc1');
+INSERT INTO `transaction` VALUES ('722ece00-7e91-4b5d-a519-2a8e9ab55cd1',120.00,'2024-05-19 15:26:32',0,'193dd0a0-bb76-4831-9ad9-9f8eded5e624');
---
+++ account-api/src/main/resources/schema.sql
@@ -0,0 +1,107 @@
+-- MySQL dump 10.13  Distrib 9.2.0, for macos15.2 (arm64)
+--
+-- Host: 192.168.68.54    Database: bank
+-- ------------------------------------------------------
+-- Server version	8.4.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+DROP TABLE IF EXISTS `transaction`;
+DROP TABLE IF EXISTS `account`;
+DROP TABLE IF EXISTS `customer`;
+
+--
+-- Table structure for table `customer`
+--
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `customer` (
+  `id` varchar(255) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `surname` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customer`
+--
+
+LOCK TABLES `customer` WRITE;
+/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `account`
+--
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `account` (
+  `id` varchar(255) NOT NULL,
+  `balance` decimal(19,2) DEFAULT NULL,
+  `creation_date` datetime DEFAULT NULL,
+  `customer_id` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKnnwpo0lfq4xai1rs6887sx02k` (`customer_id`),
+  CONSTRAINT `FKnnwpo0lfq4xai1rs6887sx02k` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `account`
+--
+
+LOCK TABLES `account` WRITE;
+/*!40000 ALTER TABLE `account` DISABLE KEYS */;
+/*!40000 ALTER TABLE `account` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `transaction`
+--
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `transaction` (
+  `id` varchar(255) NOT NULL,
+  `amount` decimal(19,2) DEFAULT NULL,
+  `transaction_date` datetime DEFAULT NULL,
+  `transaction_type` int DEFAULT NULL,
+  `account_id` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK6g20fcr3bhr6bihgy24rq1r1b` (`account_id`),
+  CONSTRAINT `FK6g20fcr3bhr6bihgy24rq1r1b` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transaction`
+--
+
+LOCK TABLES `transaction` WRITE;
+/*!40000 ALTER TABLE `transaction` DISABLE KEYS */;
+/*!40000 ALTER TABLE `transaction` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-04-10 21:14:13
